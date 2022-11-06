65a7e108c60d17bb53530324d9853432ff9f4435
Alexey Sementkovskiy <sementkovsky@yandex.ru>
2022-11-06 14:05:41 +0300

    C34

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -3,23 +3,25 @@ package ru.kata.spring.boot_security.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.kata.spring.boot_security.demo.dao.UserDao;
 import ru.kata.spring.boot_security.demo.model.User;
 import java.security.Principal;
 
 @Controller
-public class HelloController {
+@RequestMapping("/user")
+public class UserController {
     private final UserDao userDao;
 
-    public HelloController(UserDao userDao) {
+    public UserController(UserDao userDao) {
         this.userDao = userDao;
     }
 
-    @GetMapping(value = "/user")
+    @GetMapping(value = "")
     public String printWelcome(ModelMap model, Principal principal) {
         User user = userDao.findByName(principal.getName());
         model.addAttribute("messages", user);
-        return "user";
+        return "user/user";
     }
 }
 
