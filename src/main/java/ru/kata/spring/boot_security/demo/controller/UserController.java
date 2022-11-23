9f7b99e0730169bf6d131fff04a5620b362b2fb4
Alexey Sementkovskiy <sementkovsky@yandex.ru>
2022-11-23 16:45:42 +0300

    C0020

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -5,13 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import ru.kata.spring.boot_security.demo.dao.UserDao;
 import ru.kata.spring.boot_security.demo.model.User;
-
-import java.security.Principal;
 
 @Controller
 public class UserController {
@@ -21,18 +18,16 @@ public class UserController {
         this.userDao = userDao;
     }
 
+    @GetMapping(value = "/user")
+    public String showUserPage() {
+        return "user/user";
+    }
+
     @GetMapping("/api/user")
     @ResponseBody
     public ResponseEntity<User> showUser() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = (User) auth.getPrincipal();
         return new ResponseEntity<>(user, HttpStatus.OK);
-    }
-
-    @GetMapping(value = "/user")
-    public String printWelcome(ModelMap model, Principal principal) {
-        User user = userDao.findByEmail(principal.getName());
-        model.addAttribute("user", user);
-        return "user/user";
     }
 }
