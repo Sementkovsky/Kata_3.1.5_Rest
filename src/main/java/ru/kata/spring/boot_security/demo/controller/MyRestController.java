e7baeb9aba126d401b56b2da1bf05625c98397f0
Alexey Sementkovskiy <sementkovsky@yandex.ru>
2022-11-24 11:49:28 +0300

    C0023

--- src/main/java/ru/kata/spring/boot_security/demo/controller/MyRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/MyRestController.java
@@ -2,6 +2,8 @@ package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
@@ -19,29 +21,33 @@ public class MyRestController {
     }
 
     @GetMapping("/users")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<User>> getAll() {
         return new ResponseEntity<>(userService.listUsers(), HttpStatus.OK);
     }
 
     @GetMapping("/user/{id}")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<User> getUser(@PathVariable("id") Long id) {
         return new ResponseEntity<>(userService.getById(id), HttpStatus.OK);
     }
 
     @PostMapping("/new")
+    @Secured("ROLE_ADMIN")
     public ResponseEntity<User> create(@RequestBody User user) {
-        System.out.println(user);
         userService.add(user);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
         userService.removeUser(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PatchMapping("/edit/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> editUser(@PathVariable("id") Long id, @RequestBody User user) {
         user.setId(id);
         userService.updateUser(user);
